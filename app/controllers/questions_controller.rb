1bb2ba36b75d88bf36a440098226c7d0a1ba3e48
Yatin Nagpal <nnega4428@gmail.com>
2022-11-28 21:08:47 +1100

    added a route to answer created the answer view and connected the params

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,17 @@ class QuestionsController < ApplicationController
 
   def ask
   end
+
+  def answer
+    @question = params['question']
+    @ans = ques_ans(@question)
+  end
+
+  def ques_ans(question)
+    if question.end_with?('?')
+      'No! GO TO WORK'
+    elsif question == 'I am going to work now'
+      'Good job pay taxes and stay out of prison'
+    end
+  end
 end
